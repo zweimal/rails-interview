bd34194680fec828b1bdd427fbf17558930fb57d
German Allemand <zweimal@gmail.com>
2017-03-15 23:46:27 -0300

    Added missing method name to student

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,13 @@
 class Student < ActiveRecord::Base
+  def name
+    lastname_initial = " #{lastname.first}." if has_repeated_firstname
+    "#{firstname}#{lastname_initial}"
+  end
+
+  private
+  
+  def has_repeated_firstname
+    table = Student.arel_table
+    Student.where(table[:firstname].matches(firstname)).count > 1
+  end
 end
