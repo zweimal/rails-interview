735ed9e1e92f5699df24603d29af19d8f7b0e827
German Allemand <zweimal@gmail.com>
2017-03-16 00:00:00 -0300

    Added students_controller index functionality

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,20 @@
 class StudentsController < ApplicationController
   def index
-    #stuff will happen here
+    movie = student_params[:movie]
+    @students = if movie.present?
+        Student.where student_table[:favoritemovie].matches("%#{movie}%")
+      else 
+        Student.all
+      end
+  end
+
+  private
+
+  def student_params
+    params.permit(:movie)
+  end
+
+  def student_table
+    Student.arel_table
   end
 end
